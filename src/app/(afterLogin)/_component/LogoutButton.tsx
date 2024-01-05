73a66f1562cf1b0d2229742e392c3c96176734ea
young-02 <taeyoung917@gmail.com>
2024-01-06 08:28:08 +0900

    home layout

---
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Image from "next/image";
+import style from "./LogoutButton.module.css";
+
+export default function LogoutButton() {
+  const me = {
+    id: "profile",
+    nickname: "gg",
+    image: "/person.png",
+  };
+
+  const onLogout = () => {};
+  return (
+    <button className={style.logOutButton} onClick={onLogout}>
+      <div className={style.logOutUserImage}>
+        <Image
+          src={me.image}
+          alt={me.id}
+          objectFit="cover"
+          width={50}
+          height={50}
+        />
+      </div>
+      <div className={style.logOutUserName}>
+        <div>{me.nickname}</div>
+        <div>@{me.id}</div>
+      </div>
+    </button>
+  );
+}
--- src/app/(afterLogin)/home/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default async function HomeLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
-}
