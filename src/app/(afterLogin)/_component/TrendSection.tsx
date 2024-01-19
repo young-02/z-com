5d56d1031764e87fdb3b9e46a1eccedaac8f46e6
young-02 <taeyoung917@gmail.com>
2024-01-19 14:15:12 +0900

    search, useSearchParams() 메소드(get, toString)

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,6 +1,11 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Trend from "./Trend";
 import style from "./TrendSection.module.css";
+
 export default function TrendSection() {
+  const pathname = usePathname();
+  if (pathname === "/explore") return null;
   return (
     <div className={style.trendBg}>
       <div className={style.trend}>
