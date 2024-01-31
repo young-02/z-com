9d78edb3e8dcfa8adf8d398a2e1e920947aca210
young-02 <taeyoung917@gmail.com>
2024-01-31 16:39:20 +0900

    메시지 클론

---
+++ src/app/(afterLogin)/messages/_component/Room.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { faker } from "@faker-js/faker";
+import style from "../message.module.css";
+import { useRouter } from "next/navigation";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
+
+export default function Room() {
+  const router = useRouter();
+
+  const user = {
+    id: "hero",
+    nickname: "영웅",
+    Messages: [
+      { roomId: 123, content: "안녕하세요", createdAt: new Date() },
+      { roomId: 123, content: "가세요", createdAt: new Date() },
+    ],
+  };
+
+  const onClick = () => {
+    router.push(`/messages/${user.Messages.at(-1)?.roomId}`);
+  };
+  return (
+    <div className={style.room} onClick={onClick}>
+      <div className={style.roomUserImage}>
+        <img src={faker.image.avatar()} alt="" />
+      </div>
+      <div className={style.roomChatInfo}>
+        <div className={style.roomUserinfo}>
+          <b>{user.nickname}</b>
+          &nbsp
+          <span>@{user.id}</span>- &nbsp
+          <span className={style.postDate}>
+            {dayjs(user.Messages?.at(-1)?.createdAt).fromNow(true)}
+          </span>
+        </div>
+        <div className={style.roomLastChat}>
+          {user.Messages?.at(-1)?.content}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,22 @@
+import style from "./message.module.css";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Room from "./_component/Room";
+
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
+
 export default function Home() {
-  return <main>dddddd</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h2>쪽지</h2>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
